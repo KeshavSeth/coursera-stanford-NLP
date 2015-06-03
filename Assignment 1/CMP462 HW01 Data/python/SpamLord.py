a4457dbfaa001cd7fafdb18d1c4b53a8fbf3c04d
KeshavSeth <setkeshav@gmail.com>
2015-06-03 17:48:12 +0530

    final Result

--- Assignment 1/CMP462 HW01 Data/python/SpamLord.py
+++ Assignment 1/CMP462 HW01 Data/python/SpamLord.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
+
 import sys
 import os
 import re
 import pprint
 
-my_first_pat = '(\w+)@(\w+).edu'
-
+my_first_pat = '(([\w\.-]+)(\s.?\(f.*y.*)?( *(@|&.*;) *| +(at|AT|where|WHERE) +)([\w-]+|[\w-]+([\.;]| +[dD][oO]?[tT] +| +)[\w-]+)([\.;]| +(do?t|DO?[TM]) +| )(-?[eE]-?[dD]-?[uU]|com|COM)\W)|obfuscate\(\'([\w]+\.edu)\',\'([\w]+)\'\)'
+phone_pattern = '\(?(\d{3})\)?[ -]?(\d{3})[ -](\d{4})\D+'
 """
 TODO
 This function takes in a filename along with the file object (actually
@@ -26,29 +28,55 @@ NOTE: You shouldn't need to worry about this, but just so you know, the
 sure you check the StringIO interface if you do anything really tricky,
 though StringIO should support most everything.
 """
+
+
 def process_file(name, f):
     # note that debug info should be printed to stderr
     # sys.stderr.write('[process_file]\tprocessing file: %s\n' % (path))
     res = []
     for line in f:
-        matches = re.findall(my_first_pat,line)
+        matches = re.findall(my_first_pat, line)
         for m in matches:
-            email = '%s@%s.edu' % m
-            res.append((name,'e',email))
+            # print len(m)
+            if m[1] == "Server":
+                continue
+            # if name == "balaji":
+            #     print m
+            #     print len(m)
+            #     print name
+            #     print '%s@%s.%s' % (m[1].replace("-", ""),
+            #                         m[6].replace("cs ", "cs.").replace(
+            #         "-", "").replace(" ", "").replace("dot", ".").replace(";", ".").replace("..", "."),
+            #         m[10].replace("-", ""))
+            if len(m[len(m) - 1]) != 0:
+                email = "%s@%s" % (m[12], m[11])
+            else:
+                email = '%s@%s.%s' % (m[1].replace("-", ""),
+                                      m[6].replace("cs ", "cs.").replace(
+                    "-", "").replace(" ", "").replace("dot", ".").replace(";", ".").replace("..", "."),
+                    m[10].replace("-", ""))
+            res.append((name, 'e', email))
+
+        matches = re.findall(phone_pattern, line)
+        for m in matches:
+            phone = '%s-%s-%s' % m
+            res.append((name, 'p', phone))
     return res
 
 """
 You should not need to edit this function, nor should you alter
 its interface as it will be called directly by the submit script
 """
+
+
 def process_dir(data_path):
     # get candidates
     guess_list = []
     for fname in os.listdir(data_path):
         if fname[0] == '.':
             continue
-        path = os.path.join(data_path,fname)
-        f = open(path,'r')
+        path = os.path.join(data_path, fname)
+        f = open(path, 'r')
         f_guesses = process_file(fname, f)
         guess_list.extend(f_guesses)
     return guess_list
@@ -59,10 +87,12 @@ Given a path to a tsv file of gold e-mails and phone numbers
 this function returns a list of tuples of the canonical form:
 (filename, type, value)
 """
+
+
 def get_gold(gold_path):
     # get gold answers
     gold_list = []
-    f_gold = open(gold_path,'r')
+    f_gold = open(gold_path, 'r')
     for line in f_gold:
         gold_list.append(tuple(line.strip().split('\t')))
     return gold_list
@@ -74,9 +104,13 @@ computes the intersection and set differences, to compute the true
 positives, false positives and false negatives.  Importantly, it
 converts all of the values to lower case before comparing
 """
+
+
 def score(guess_list, gold_list):
-    guess_list = [(fname, _type, value.lower()) for (fname, _type, value) in guess_list]
-    gold_list = [(fname, _type, value.lower()) for (fname, _type, value) in gold_list]
+    guess_list = [(fname, _type, value.lower())
+                  for (fname, _type, value) in guess_list]
+    gold_list = [(fname, _type, value.lower())
+                 for (fname, _type, value) in gold_list]
     guess_set = set(guess_list)
     gold_set = set(gold_list)
 
@@ -85,26 +119,30 @@ def score(guess_list, gold_list):
     fn = gold_set - guess_set
 
     pp = pprint.PrettyPrinter()
-    #print 'Guesses (%d): ' % len(guess_set)
-    #pp.pprint(guess_set)
-    #print 'Gold (%d): ' % len(gold_set)
-    #pp.pprint(gold_set)
-    print 'True Positives (%d): ' % len(tp)
-    pp.pprint(tp)
-    print 'False Positives (%d): ' % len(fp)
-    pp.pprint(fp)
+    # print 'Guesses (%d): ' % len(guess_set)
+    # pp.pprint(guess_set)
+    # print 'Gold (%d): ' % len(gold_set)
+    # pp.pprint(gold_set)
     print 'False Negatives (%d): ' % len(fn)
     pp.pprint(fn)
-    print 'Summary: tp=%d, fp=%d, fn=%d' % (len(tp),len(fp),len(fn))
+    # print 'True Positives (%d): ' % len(tp)
+    # pp.pprint(tp)
+    print 'False Positives (%d): ' % len(fp)
+    pp.pprint(fp)
+    # print 'False Negatives (%d): ' % len(fn)
+    # pp.pprint(fn)
+    print 'Summary: tp=%d, fp=%d, fn=%d' % (len(tp), len(fp), len(fn))
 
 """
 You should not need to edit this function.
 It takes in the string path to the data directory and the
 gold file
 """
+
+
 def main(data_path, gold_path):
     guess_list = process_dir(data_path)
-    gold_list =  get_gold(gold_path)
+    gold_list = get_gold(gold_path)
     score(guess_list, gold_list)
 
 """
@@ -116,7 +154,7 @@ if __name__ == '__main__':
     if (len(sys.argv) == 1):
         main('../data/dev', '../data/devGOLD')
     elif (len(sys.argv) == 3):
-        main(sys.argv[1],sys.argv[2])
+        main(sys.argv[1], sys.argv[2])
     else:
         print 'usage:\tSpamLord.py <data_dir> <gold_file>'
         sys.exit(0)
